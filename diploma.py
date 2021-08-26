d1bfa64378ab30c2c854f845d8cf9e1f9dd44a2f
Dzhentlmen <88185091+Dzhentlmen@users.noreply.github.com>
2021-08-26 15:36:43 +0300

    Update diploma.py

--- diploma.py
+++ diploma.py
@@ -12,6 +12,7 @@ from progress.bar import Bar
 #VK Program Token
 token = '0cce3a725e6316f5f33bcb6ce1af7133f58e0d1c6aea1268ae56c63b5837e43b74682a33b8f8be4d91c26'
 
+#TOKEN = 'AQAAAAA0PqSxAADLW0QNkVCS60kIkwupvRZDqUk'
 #Получение данных от пользователя
 #Getting data from the user
 input_screen_name = input('Введите имя пользователя: ')
@@ -60,8 +61,8 @@ for i in download:
   dict_url_likes[i['sizes'][:][-1]['url']] = i['likes']['count']
   list_json.append({'file_name': i['likes']['count']})
 
-#Индикатор выполнения
-#Progress Bar
+# Индикатор выполнения
+# Progress Bar
 bar = Bar('Загрузка фотографий на Я.Диск...', max = len(dict_url_likes.values()), suffix = '%(percent)d%%', fill = '█')
 for img_url, likes in dict_url_likes.items():
   #Загрузка на Я.Диск
@@ -72,14 +73,11 @@ for img_url, likes in dict_url_likes.items():
     'Authorization': f'OAuth {input_token}'
   }
   requests.post(API_BASE_URL + 'v1/disk/resources/upload', headers = headers, params = {'path': 'diploma/' + str(likes), 'url': img_url})
-
-  #Запись логов о загрузке на Я.Диск в файл log.json
-  #Logging about the upload to a Yandex.Disk in file "log.json"
-  with open('log.json', 'a') as file:
-    for dict_file in list_json:
-      dict_file['size'] = i['sizes'][:][-1]['type']
-    file.write(now.strftime("%d-%m-%Y %H:%M") + ': ' + str(list_json))
-    file.write('\n')
   bar.next()
+# Запись логов о загрузке на Я.Диск в файл log.json
+  # Logging about the upload to a Yandex.Disk in file "log.json"
+with open('log.json', 'a') as file:
+  file.write(now.strftime("%d-%m-%Y %H:%M") + ': ' + str(list_json))
+  file.write('\n')
 bar.finish()
 print('Готово! Фотография(-ии) загружены')
